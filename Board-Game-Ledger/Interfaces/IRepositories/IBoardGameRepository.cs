92ecc32aa95e7e98c2a0a5f6acba43b51403e911
Jzerior <jakubjezior@gmail.com>
2025-05-13 22:14:40 +0200

    Initial interfaces of repositories

---
+++ Board-Game-Ledger/Interfaces/IRepositories/IBoardGameRepository.cs
@@ -0,0 +1,14 @@
+﻿using Board_Game_Ledger.Models;
+
+namespace Board_Game_Ledger.Interfaces.IRepositories
+{
+    public interface IBoardGameRepository
+    {
+        Task<List<BoardGame>> GetAllAsync();
+        Task<BoardGame?> GetByIdAsync(int id);
+        Task<BoardGame?> GetByNameAsync(string name);
+        Task<BoardGame> CreateAsync(BoardGame boardGame);
+        //Task<BoardGame?> UpdateAsync(int id,BoardGame boardGame);
+        Task<BoardGame?> DeleteAsync(int id);
+    }
+}
---
+++ Board-Game-Ledger/Interfaces/IRepositories/IPlayerRepostiory.cs
@@ -0,0 +1,14 @@
+﻿using Board_Game_Ledger.Models;
+
+namespace Board_Game_Ledger.Interfaces.IRepositories
+{
+    public interface IPlayerRepostiory
+    {
+        Task<List<Player>> GetAllAsync();
+        Task<Player?> GetByIdAsync(int id);
+        Task<Player?> GetByNameAsync(string name);
+        Task<Player> CreateAsync(BoardGame boardGame);
+        //Task<Player?> UpdateAsync(int id,BoardGame boardGame);
+        Task<Player?> DeleteAsync(int id);
+    }
+}
